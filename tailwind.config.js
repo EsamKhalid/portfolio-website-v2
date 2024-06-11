ca6c2881b3ab944e76f2a541a107eec136ae86fd
Esam Khalid <esam-khalid@outlook.com>
2024-06-11 20:07:24 +0100

    added the untracked files from the last commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Blender-Pro-Thin' :['Blender-Pro-Thin'],
+        'Blender-Pro-Medium' :['Blender-Pro-Medium'],
+        'Blender-Pro-Heavy' :['Blender-Pro-Heavy'],
+        'Blender-Pro-Book' :['Blender-Pro-Book'],
+      },
+      
+      colors:{
+        'cybpnk-grey' : '#21201f',
+        'cybpnk-alt' : '#252826',
+        'cybpnk-blk' : '#131212',
+        'cybpnk-ylw' : '#eabc49',
+        'cybpnk-blk2' : '#171619',
+      }
+
+    },
+  },
+  plugins: [],
+}
+
