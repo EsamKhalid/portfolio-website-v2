ae839cf4a0037d3dfe307bc6a4191de9a6f1d284
Esam Khalid <esam-khalid@outlook.com>
2024-06-19 18:09:21 +0100

    started outline of project tile

--- components/ProjectsSection/ProjectTile.tsx
+++ components/ProjectsSection/ProjectTile.tsx
@@ -6,9 +6,12 @@ import placeholder from "../../Images/placeholder.avif";
 export default function ProjectTile() {
   return (
     <>
-      <div className="inline-block border-cybpnk-brdr p-2">
+      <div className="inline-block border border-cybpnk-brdr p-2 [&_*]:p-2">
         <h1 className="text-4xl">Project One</h1>
         <Image className="w-[250px] h-[250px]" src={placeholder} alt="image" />
+        <h2>Type: Personal Project</h2>
+        <h2>Start Date: 19/06/24</h2>
+        <h2>End Date: 29/06/25</h2>
       </div>
     </>
   );
