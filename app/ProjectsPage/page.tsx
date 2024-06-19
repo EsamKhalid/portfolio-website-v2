0a1180283b93f9ccf6f4a99de009f24434ef31e1
Esam Khalid <esam-khalid@outlook.com>
2024-06-19 21:16:25 +0100

    got props to work with grid tile component

--- app/ProjectsPage/page.tsx
+++ app/ProjectsPage/page.tsx
@@ -2,30 +2,53 @@ import React from "react";
 import Image from "next/image";
 
 import NavBar from "@/components/NavBar/NavBar";
+import ProjectTile from "@/components/ProjectsSection/ProjectTile";
 
 import placeholder from "../../Images/placeholder.avif";
 
-import ProjectTile from "@/components/ProjectsSection/ProjectTile";
-
 export default function Page() {
-  const test = [<h1>Test</h1>];
-
   return (
     <>
-      {test[0]}
       <NavBar />
       <div className="px-[25px] pt-[15px]">
         <h1 className="text-4xl pb-[25px]">// Projects Page</h1>
         <div className="flex justify-center">
           <div className="grid grid-cols-4 gap-5">
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
-            <ProjectTile />
+            <ProjectTile
+              ProjectTitle="Project One"
+              ImageSource={placeholder}
+              ProjectType="Personal Project"
+              StartDate="19/06/24"
+              EndDate="29/06/25"
+            />
+            <ProjectTile
+              ProjectTitle="Project One"
+              ImageSource={placeholder}
+              ProjectType="Personal Project"
+              StartDate="19/06/24"
+              EndDate="29/06/25"
+            />
+            <ProjectTile
+              ProjectTitle="Project One"
+              ImageSource={placeholder}
+              ProjectType="Personal Project"
+              StartDate="19/06/24"
+              EndDate="29/06/25"
+            />
+            <ProjectTile
+              ProjectTitle="Project One"
+              ImageSource={placeholder}
+              ProjectType="Personal Project"
+              StartDate="19/06/24"
+              EndDate="29/06/25"
+            />
+            <ProjectTile
+              ProjectTitle="Project One"
+              ImageSource={placeholder}
+              ProjectType="Personal Project"
+              StartDate="19/06/24"
+              EndDate="29/06/25"
+            />
           </div>
         </div>
       </div>
--- components/ProjectsSection/ProjectTile.tsx
+++ components/ProjectsSection/ProjectTile.tsx
@@ -1,17 +1,35 @@
 import React from "react";
 
-import Image from "next/image";
-import placeholder from "../../Images/placeholder.avif";
+import Image, { StaticImageData } from "next/image";
 
-export default function ProjectTile() {
+interface ProjectTileProps {
+  ProjectTitle: string;
+  ProjectType: string;
+  StartDate: string;
+  EndDate: string;
+  ImageSource: StaticImageData;
+}
+
+export default function ProjectTile({
+  ProjectTitle,
+  ProjectType,
+  StartDate,
+  EndDate,
+  ImageSource,
+}: ProjectTileProps) {
   return (
     <>
       <div className="inline-block border border-cybpnk-brdr p-2 [&_*]:p-2">
-        <h1 className="text-4xl">Project One</h1>
-        <Image className="w-[250px] h-[250px]" src={placeholder} alt="image" />
-        <h2>Type: Personal Project</h2>
-        <h2>Start Date: 19/06/24</h2>
-        <h2>End Date: 29/06/25</h2>
+        <h1 className="text-4xl">{ProjectTitle}</h1>
+        <Image
+          fill={false}
+          className="w-[250px] h-[250px]"
+          src={ImageSource}
+          alt="image"
+        />
+        <h2>Type: {ProjectType}</h2>
+        <h2>Start Date: {StartDate}</h2>
+        <h2>End Date: {EndDate}</h2>
       </div>
     </>
   );
