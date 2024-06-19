040171f8d8f8d52a76f2b57f61cb7968342743ab
Esam Khalid <esam-khalid@outlook.com>
2024-06-19 21:06:37 +0100

    got grid to work, starting implementing props->gridTiles

--- app/ProjectsPage/page.tsx
+++ app/ProjectsPage/page.tsx
@@ -8,12 +8,26 @@ import placeholder from "../../Images/placeholder.avif";
 import ProjectTile from "@/components/ProjectsSection/ProjectTile";
 
 export default function Page() {
+  const test = [<h1>Test</h1>];
+
   return (
     <>
+      {test[0]}
       <NavBar />
       <div className="px-[25px] pt-[15px]">
         <h1 className="text-4xl pb-[25px]">// Projects Page</h1>
-        <ProjectTile />
+        <div className="flex justify-center">
+          <div className="grid grid-cols-4 gap-5">
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+            <ProjectTile />
+          </div>
+        </div>
       </div>
     </>
   );
