267709b54822dd837cf297a0f86d70f19e11833c
Esam Khalid <esam-khalid@outlook.com>
2024-06-20 08:39:21 +0100

    can sort dates and render child components through list

--- components/ProjectsSection/SortableGrid.tsx
+++ components/ProjectsSection/SortableGrid.tsx
@@ -11,17 +11,37 @@ interface SortableGridProps {
 }
 
 export default function SortableGrid({ children }: SortableGridProps) {
-  const dateList: Date[] = [];
+  const dateList: number[] = [];
+
+  const test: React.ReactNode[] = [];
+
+  const tempArr: number[] = [];
 
   React.Children.map(children, (child) => {
     if (React.isValidElement(child) && child.type === ProjectTile) {
-      dateList.push(new Date(child.props.StartDate));
+      dateList.push(new Date(child.props.StartDate).getTime());
+
+      dateList.sort(function (a, b) {
+        return a - b;
+      });
+
+      const date = new Date(child.props.StartDate);
+
+      const dindex = dateList.indexOf(date.getTime());
+
+      if (tempArr[dindex] != undefined) {
+        tempArr[dindex + 1] = dateList.indexOf(date.getTime());
+      } else {
+        tempArr[dindex] = dateList.indexOf(date.getTime());
+      }
+
+      test.push(child);
     }
   });
 
   for (let i = 0; i < dateList.length; i++) {
-    console.log(dateList[i]);
+    console.log(new Date(dateList[i]));
   }
 
-  return <div className="grid grid-cols-4 gap-5">{children}</div>;
+  return <div className="grid grid-cols-4 gap-5">{test}</div>;
 }
