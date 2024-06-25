407df96270b583b00d5618abd3b98e87a2034e6a
Esam Khalid <esam-khalid@outlook.com>
2024-06-25 02:53:15 +0100

    added animation for project hover

--- components/ProjectsSection/ProjectTile.tsx
+++ components/ProjectsSection/ProjectTile.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Image, { StaticImageData } from "next/image";
 
 import DiagonalLines from "../Shapes/DiagonalLines";
 import CornerBorder from "../Borders/CornerBorder";
+import { motion } from "framer-motion";
 
 interface ProjectTileProps {
   ProjectTitle: string;
@@ -22,36 +23,45 @@ export default function ProjectTile({
   ImageSource,
   className,
 }: ProjectTileProps) {
+  const [isHovered, setHovered] = useState(false);
   return (
     <>
-      <CornerBorder
-        className={"min-h-fit min-w-fit " + className}
-        cornerSize="15px"
-        borderThickness="2px"
-        borderGap="3px"
+      <motion.div
+        className="hover:cursor-pointer"
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        animate={{ scale: isHovered ? 1.05 : 1 }}
+        transition={{ duration: 0.2 }}
       >
-        <div
-          className="inline-block border border-cybpnk-brdr rounded  p-2 [&_*]:p-1 md:[&_*]:p-2
-        min-h-full min-w-fit"
+        <CornerBorder
+          className={"min-h-fit min-w-fit " + className}
+          cornerSize="15px"
+          borderThickness="2px"
+          borderGap="3px"
         >
-          <h1 className="text-4xl text-nowrap">{"// " + ProjectTitle}</h1>
-          <Image
-            fill={false}
-            className="sm:w-[200px] sm:h-[200px] md:min-w-[250px]  md:min-h-[250px]"
-            src={ImageSource}
-            alt="image"
-          />
-          <h2>//Type: {ProjectType}</h2>
-          <h2>//Start_Date: {new Date(StartDate).toLocaleDateString()}</h2>
-          <h2>//End_Date: {EndDate}</h2>
-          <DiagonalLines
-            className="mt-[10px] h-[18px]"
-            bgsize="25px"
-            rotation="right"
-            thicknessType="thick"
-          />
-        </div>
-      </CornerBorder>
+          <div
+            className="inline-block border border-cybpnk-brdr rounded  p-2 [&_*]:p-1 md:[&_*]:p-2
+        min-h-full min-w-fit"
+          >
+            <h1 className="text-4xl text-nowrap">{"// " + ProjectTitle}</h1>
+            <Image
+              fill={false}
+              className="sm:w-[200px] sm:h-[200px] md:min-w-[250px]  md:min-h-[250px]"
+              src={ImageSource}
+              alt="image"
+            />
+            <h2>//Type: {ProjectType}</h2>
+            <h2>//Start_Date: {new Date(StartDate).toLocaleDateString()}</h2>
+            <h2>//End_Date: {EndDate}</h2>
+            <DiagonalLines
+              className="mt-[10px] h-[18px]"
+              bgsize="25px"
+              rotation="right"
+              thicknessType="thick"
+            />
+          </div>
+        </CornerBorder>
+      </motion.div>
     </>
   );
 }
--- components/SectionNavigation/NavElement.tsx
+++ components/SectionNavigation/NavElement.tsx
@@ -8,28 +8,6 @@ import FlatCornerBox from "../Shapes/FlatCornerBox";
 interface valueProps {
   Value1: String;
 }
-/*
-export default function NavElement({ Value1 }: valueProps) {
-  const [isHovered, setHovered] = useState(false);
-  return (
-    <div className="flex items-center inhe p-2 [&_#hover]:hidden">
-      <FaMinus id="default" />
-      <FaArrowRight id="hover" />
-      <motion.div
-        className="ml-[2px]"
-        onMouseEnter={() => setHovered(true)}
-        onMouseLeave={() => setHovered(false)}
-        animate={{ x: isHovered ? 10 : 0 }}
-      >
-        <Link href={"#" + Value1}>
-          <h1>{Value1}</h1>
-        </Link>
-      </motion.div>
-    </div>
-  );
-}
-
-*/
 
 export default function NavElement({ Value1 }: valueProps) {
   const [isHovered, setHovered] = useState(false);
