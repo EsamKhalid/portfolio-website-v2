f1753e3b442482bb91275d85b9a169a8bab499bf
Esam Khalid <esam-khalid@outlook.com>
2024-06-28 14:58:52 +0100

    got the button styling and positioning done

--- components/Carousel/LanguageCarousel.tsx
+++ components/Carousel/LanguageCarousel.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import React from "react";
+
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 interface LanguageCarouselProps {
   Cards: JSX.Element[];
 }
 
 export default function LanguageCarousel({ Cards }: LanguageCarouselProps) {
+  const [currentCard, setCurrentCard] = React.useState(0);
+
   return (
     <>
-      <div></div>
+      <div className="relative">
+        {Cards[currentCard]}
+        <button className="carouselBtn left-0">
+          <FaArrowLeft />
+        </button>
+        <button className="carouselBtn right-0">
+          <FaArrowRight />
+        </button>
+      </div>
     </>
   );
 }
