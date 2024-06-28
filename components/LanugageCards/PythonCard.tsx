d9dd823aa0fde236f487fe9a48726aa2685636ad
Esam Khalid <esam-khalid@outlook.com>
2024-06-28 01:17:24 +0100

    finished basic skeleton for language card to use in carousel

--- components/LanugageCards/PythonCard.tsx
+++ components/LanugageCards/PythonCard.tsx
@@ -9,9 +9,9 @@ import { FaStarHalf } from "react-icons/fa";
 export default function PythonCard() {
   return (
     <>
-      <SectionBorderLines id="" className="rounded">
-        <div className="flex justify-between">
-          <FaPython className="size-[100px] m-5" />
+      <SectionBorderLines id="" className="rounded w-[60vw]">
+        <div className="flex justify-evenly">
+          <FaPython className="size-[100px] m-5 ml-12" />
           <h1 className="text-4xl text-center m-[50px]">Python</h1>
         </div>
         <div className="flex justify-evenly">
@@ -22,6 +22,15 @@ export default function PythonCard() {
             <FaStar />
             <FaStar />
           </div>
+        </div>
+        <div className="">
+          <p className="border border-cybpnk-brdr p-2 m-10 ">
+            I first started learning Python in secondary school, where we learnt
+            simple programming concepts, <br />
+            I then chose to study Computer Science at A-Level where I learnt
+            python in more detail including the use of python in both functional
+            and object oriented programming. <br />
+          </p>
         </div>
       </SectionBorderLines>
     </>
