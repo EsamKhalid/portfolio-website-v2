7eb50aa14016eb48a193f5d1d438c1a0143e5ec6
Esam Khalid <esam-khalid@outlook.com>
2024-07-01 01:26:22 +0100

    finished hobbies and interests section

--- app/AboutPage/page.tsx
+++ app/AboutPage/page.tsx
@@ -33,7 +33,7 @@ export default function page() {
           <div className="inline-block justify-center">
             <SectionBorderLines id="" className="w-fit">
               <h1 className="text-4xl">//Introduction</h1>
-              <p className="text-xl">
+              <p className="text-xl text-justify">
                 Hello! I'm <span className="text-cybpnk-ylw2">Esam Khalid</span>
                 , a passionate and dedicated computer science student at Aston
                 University, currently pursuing a BSc Hons in Computer Science
@@ -94,6 +94,58 @@ export default function page() {
                   </li>
                 </ul>
               </div>
+            </SectionBorderLines>
+            <div className="h-[10vh]"></div>
+            <SectionBorderLines id="" className="w-fit text-left">
+              <h1 className="text-4xl text-center">//Hobbies and Interests</h1>
+              <h2 className="text-2xl text-left mb-2">//Bouldering</h2>
+              <p className="text-lg text-justify mx-5">
+                I started bouldering in April of 2024, and I have loved every
+                second of it. It has been a great way to keep fit and stay
+                active, especially in a field like computer science where a
+                majority of my time would be spent behind a screen working hard
+                on a side project or learning new things. It has also taught me
+                a lot and shares similarities between computer science such as:
+                Problem solving, Incremental progress and iteration, Creative
+                Thinking and Attention to Detail just to name a few. These
+                similarities have helped me to improve in both bouldering and
+                computer science as what I learn from one can be applied to the
+                other.
+              </p>
+              <h2 className="text-2xl text-left mb-2">//Video Games</h2>
+              <p className="text-lg text-justify mx-5">
+                I am an avid gamer and have been playing video games for most of
+                my life since my first ever console the Nintendo Wii. Video
+                games are what originally sparked an interest in coding because
+                it has always been a dream to one day make a game of my own that
+                not only I would enjoy but also other players. Over the years I
+                have realised that my favourite way to practice coding and
+                coding concepts would be to make games from it ranging from text
+                based in secondary school to pygame and unity in sixth form. My
+                love for making games made me learn to code in C# to use in the
+                unity engine which has been a great help in then learning to
+                code in java in the first year of university and overall
+                solidified my knowledge and experince with coding in general.
+              </p>
+              <h2 className="text-2xl text-left mb-2">//Coding</h2>
+              <p className="text-lg text-justify mx-5">
+                My passion and love for coding has spanned much further than my
+                degree and career and has implemented itself as one of my
+                favourite hobbies and past times. Well before choosing to pursue
+                computer science as a career I would spend time building
+                something with code, starting in secondary school where I would
+                make text based games using what we learned in class and then in
+                sixth form I would make small projects in unity where one of
+                them ended up being used as my A-Level coursework. That love for
+                coding is still alive and well today and has over the years
+                gotten stronger, I am constantly learning new things and
+                thinking of cool ideas for a project that I can work on by
+                learning new things then using the new knowledge to bring the
+                idea to life through the use of code. The more I spend my time
+                coding the more I realise I am so blessed to be able to turn my
+                passion into a career and I am so excited to see where it takes
+                me in the future.
+              </p>
             </SectionBorderLines>
             <div className="h-[10vh]"></div>
           </div>
