e7f8a7af767eb7c4cb06acc9f48cc1c427ae9e7b
Esam Khalid <esam-khalid@outlook.com>
2024-07-05 22:07:09 +0100

    going to try different layout

--- components/ProjectsSection/Project.tsx
+++ components/ProjectsSection/Project.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Image, { StaticImageData } from "next/image";
 
 import NavBar from "../NavBar/NavBar";
+import DiagonalLines from "../Shapes/DiagonalLines";
 
 interface ProjectProps {
   ProjectName: string;
@@ -21,11 +22,17 @@ export default function Project({
       <div className="px-[25px] pt-[15px] overflow-hidden">
         <h1 className="text-4xl pb-[25px]">// {ProjectName}</h1>
         <div className="flex justify-center ">
-          <div className="hidden sm:block text-3xl mr-24 [&_*]:my-0">
-            <div className="flex flex-col justify-evenly size-full">
+          <div className="hidden sm:block text-3xl mr-24">
+            <div className="flex flex-col justify-between size-full border border-cybpnk-brdr p-[5%]">
               <h1>// Project_Type : {ProjectType}</h1>
               <h1>// Project_Name : {ProjectName}</h1>
               <h1>// End_Date : {ProjectName}</h1>
+              <DiagonalLines
+                className="w-full end"
+                bgsize="15px"
+                rotation="right"
+                thicknessType="thin"
+              />
             </div>
           </div>
           <div className="flex flex-col sm:flex-row justify-center md:size-[45%]  ">
@@ -34,7 +41,6 @@ export default function Project({
               src={ProjectImage}
               alt=""
             />
-
             <div className="size-full sm:hidden">
               <h1>Test</h1>
             </div>
