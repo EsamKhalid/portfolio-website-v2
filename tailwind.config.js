7753e0e2fc84e4d3275dcde1605f703ba3fdf38b
Esam Khalid <100537529+EsamKhalid@users.noreply.github.com>
2025-02-26 06:36:23 +0000

    installed dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,42 @@
-const { plugin } = require('postcss');
+const { plugin } = require("postcss");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
-      fontFamily:{
-        'Blender-Pro-Thin' :['Blender-Pro-Thin'],
-        'Blender-Pro-Medium' :['Blender-Pro-Medium'],
-        'Blender-Pro-Heavy' :['Blender-Pro-Heavy'],
-        'Blender-Pro-Book' :['Blender-Pro-Book'],
-        'Nemesys' :['nemesys'],
+      fontFamily: {
+        "Blender-Pro-Thin": ["Blender-Pro-Thin"],
+        "Blender-Pro-Medium": ["Blender-Pro-Medium"],
+        "Blender-Pro-Heavy": ["Blender-Pro-Heavy"],
+        "Blender-Pro-Book": ["Blender-Pro-Book"],
+        Nemesys: ["nemesys"],
       },
-      
-      colors:{
-        'cybpnk-grey' : '#21201f',
-        'cybpnk-alt' : '#252826',
-        'cybpnk-blk' : '#131212',
-        'cybpnk-ylw' : '#eabc49',
-        'cybpnk-ylw2' : '#fbbf24',
-        'cybpnk-blk2' : '#171619',
-        'cybpnk-brdr' : '#25252a',
-        'jms-blue' : '#14141a',
+
+      colors: {
+        "cybpnk-grey": "#21201f",
+        "cybpnk-alt": "#252826",
+        "cybpnk-blk": "#131212",
+        "cybpnk-ylw": "#eabc49",
+        "cybpnk-ylw2": "#fbbf24",
+        "cybpnk-blk2": "#171619",
+        "cybpnk-brdr": "#25252a",
+        "jms-blue": "#14141a",
       },
 
       animation: {
-        'infinite-scroll': 'infinite-scroll 5s linear infinite',
+        "infinite-scroll": "infinite-scroll 5s linear infinite",
       },
       keyframes: {
-        'infinite-scroll': {
-          from: { transform: 'translateX(0)' },
-          to: { transform: 'translateX(-100%)' },
-        }
+        "infinite-scroll": {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
-
-
     },
   },
-  plugins: [
-
-  ],
-}
-
+  plugins: [],
+};
